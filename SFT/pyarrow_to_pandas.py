f002ea50eade3eb3c3669b5ee9fa31e152c53a3c
puras <puras.he@gmail.com>
2025-02-28 15:19:17 +0800

    feat: 移除无用的文件

--- SFT/pyarrow_to_pandas.py
+++
@@ -1,8 +0,0 @@
-import pyarrow.parquet as pq
-import pandas as pd
-
-parquet_file = pq.ParquetFile("../datasets/instruction_dataset.parquet")
-data = parquet_file.read().to_pandas()
-
-print(data.head())
-data.to_csv('data.csv', index=False)
