e4e23f2ac202b1fc768ee5f3cc27310255338bdb
puras <puras.he@gmail.com>
2025-03-10 18:10:51 +0800

    update

--- demo_vllm/demo.py
+++ demo_vllm/demo.py
@@ -1,11 +1,22 @@
 from openai import OpenAI
 
-openai_api_key = ""
+openai_api_key = "EMPTY"
 openai_api_base = "http://127.0.0.1:8000/v1"
 client = OpenAI(
     api_key=openai_api_key,
     base_url=openai_api_base,
 )
-completion = client.completions.create(model="Qwen/Qwen2.5-1.5B-Instruct",
-                                      prompt="San Francisco is a")
+completion = client.completions.create(
+    model="Qwen/Qwen2.5-1.5B-Instruct",
+    prompt="你好", 
+    temperature=0,
+    )
+print("Completion result:", completion)
+completion = client.chat.completions.create(
+    model="Qwen/Qwen2.5-1.5B-Instruct",
+    messages=[
+        {"role": "user", "content": "你好"}
+    ], 
+    temperature=0,
+    )
 print("Completion result:", completion)
--- demo_vllm/main.py
+++ demo_vllm/main.py
@@ -5,12 +5,16 @@ prompts = [
     "The president of the United States is",
     "The capital of France is",
     "The future of AI is",
+    "你好",
+    "你是谁",
+    "9.11与9.9哪个更大",
 ]
 
 sampling_params = SamplingParams(temperature=0.8, top_p=0.95)
 
 # llm = LLM(model='facebook/opt-125m')
-llm = LLM(model='deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B')
+# llm = LLM(model='deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B')
+llm = LLM(model='Qwen/Qwen2.5-1.5B-Instruct')
 
 outputs = llm.generate(prompts, sampling_params)
 for output in outputs:
