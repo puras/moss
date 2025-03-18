778519aa39d92af2f7e282da2d4b8a5c31f5adf6
puras <puras.he@gmail.com>
2025-03-18 22:04:09 +0800

    add prompt

--- api/app.py
+++ api/app.py
@@ -3,6 +3,7 @@ from database import init_db
 from kb import kb_bp
 from model import model_bp
 from model_type import model_type_bp
+from prompt import prompt_bp
 
 app = Flask(__name__)
 
@@ -13,6 +14,7 @@ init_db()
 app.register_blueprint(kb_bp)
 app.register_blueprint(model_bp)
 app.register_blueprint(model_type_bp)
+app.register_blueprint(prompt_bp)
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
